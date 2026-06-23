c166bbd7ed153ef845c2e0ce3deeb2ed3c5d1285
Akshayadevi <muthukumarakshayadevi@gmail.com>
2026-06-23 18:55:21 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class dowhile{
+  public static void main(String[]args){
+     int a=1;
+     do{
+       System.out.println(a);
+       a++;
+     }
+     while(a<=10);
+   }
+}
